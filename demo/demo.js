8d60fad62cc5749381ac43a4608859daa934cada
Hank <hank.tsunghanlin@gmail.com>
2022-05-03 16:44:11 +0800

    05031644

--- demo/demo.js
+++ demo/demo.js
@@ -8,14 +8,14 @@ function closeSearch() {
 
 
 function openNav() {
-    if(document.getElementById("mySidenav").style.width == "250px"){
+    if (document.getElementById("mySidenav").style.width == "250px") {
         closeNav();
-    }else{
+    } else {
         document.getElementById("mySidenav").style.width = "250px";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
     }
-    
-    
+
+
 }
 
 function closeNav() {
@@ -28,13 +28,33 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
 }
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () { myFunction() };
+
+// Get the navbar
+var myNavBar = document.getElementById("myNavBar");
+
+// Get the offset position of the navbar
+var sticky = myNavBar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        myNavBar.classList.add("sticky")
+    } else {
+        myNavBar.classList.remove("sticky");
+    }
+}
+
